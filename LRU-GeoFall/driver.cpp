13e04f2365c62d4921b47f1dbe441bcd2426fa8a
Pavan Kotak <75493568+blueydragoon@users.noreply.github.com>
2021-06-15 22:44:22 +0530

    Added Geometric falling Scoring system

---
+++ LRU-GeoFall/driver.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include "ll.h"
+using namespace std;
+int main(){
+    int n = 3;
+    double fallFac = 0.9;
+    fstream my_file;
+    fstream fout;
+    fout.open("outputexample.csv", ios::out | ios::trunc);
+    fout << "size" << ", "
+         << "hits" << ", "
+         << "misses" << ", "
+         << "hitRatio" << ", "
+         << "missRatio" << ", "
+         << "\n";
+    while(n < 50){
+        circleLL list(n,fallFac);
+        my_file.open("../traces/trace03", ios::in);
+	    if (!my_file) {
+	        cout << "No such file";
+	    }
+        else {
+		    char ch;
+            char ch2;
+            while (1) {
+			    my_file >> ch;
+                my_file >> ch2;
+			    if (my_file.eof())
+				    break;
+                int out = 10 * (ch - '0') + ch2 - '0';
+                list.insert(out);
+            }
+	    }
+        my_file.close();
+        double hitratio = (1.00 * list.hit)/5000.00;
+        double missratio = (1.00 * list.miss)/5000.00;
+        fout << n << ", "
+             << fallFac<< ","
+             << list.hit << ", "
+             << list.miss << ", "
+             << hitratio << ", "
+             << missratio << ", "
+             << "\n";
+        n++;
+    }
+    return 0;
+}
---
+++ LRU-GeoFall/ll.h
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    double score;
+    ListNode *next;
+    ListNode *prev;
+    ListNode() : val(0), score(0), next(nullptr) {}
+    ListNode(int x) : val(x), score(0), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), score(0), next(next) {}
+};
+
+class circleLL
+{
+private:
+
+public:
+    ListNode *head;
+    ListNode *bottom;
+    int hit = 0;
+    int miss = 0;
+    double fallFac;
+    map<int, bool> inCache;
+    map<int, bool> coldMiss;
+    circleLL(int n,double w);
+    void display();
+    void insert(int entry);
+    void replace(int entry);
+    void find(int entry);
+    void update(int entry);
+    void geoFall(int entry);
+    ~circleLL();
+};
+
+circleLL::circleLL(int n,double w)
+{
+    fallFac = w;
+    head = new ListNode;
+    bottom = new ListNode;
+    ListNode *prev = bottom;
+    ListNode *curr = head;
+    for (int i = 0; i < n;i++){
+        curr->prev = prev;
+        if(i<n-2){
+            curr->next = new ListNode;
+        }
+        else if (i == n-2)
+        {
+            curr->next = bottom;
+        }
+        else if (i == n-1)
+        {
+            curr->next = head;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+void circleLL::display()
+{
+    ListNode *curr = head;
+    cout << curr->val << " " << curr->score <<endl;
+    curr = curr->next;
+    while(curr != head){
+        cout << curr->val << " " << curr->score <<endl;
+        curr = curr->next;
+    }
+}
+
+void circleLL::insert(int entry){
+    geoFall(entry);
+    if(inCache[entry]){
+        hit++;
+        update(entry);
+    }
+    else if(bottom->val == 0){
+        if(coldMiss[entry]){
+            miss++;
+        }
+        else{
+            coldMiss[entry] = true;
+        }
+        bottom->val = entry;
+        bottom->score = 1;
+        head = bottom;
+        bottom = bottom->prev;
+        inCache[entry] = true;
+    }
+    else{
+        if(coldMiss[entry]){
+            miss++;
+        }
+        else{
+            coldMiss[entry] = true;
+        }
+        replace(entry);
+    }
+}
+
+void circleLL::geoFall(int entry){
+    ListNode *curr = head;
+    while (curr->val != 0)
+    {
+        curr->score *= fallFac;
+        if(curr->next == head){
+            break;
+        }
+        else{
+            curr = curr->next;
+        }
+    }
+}
+
+void circleLL::replace(int entry){
+    while(bottom->score > 1.00){
+        head = bottom;
+        bottom = bottom->prev;
+    }
+    inCache[bottom->val] = false;
+    inCache[entry] = true;
+    ListNode *curr = new ListNode(entry);
+    curr->score = 1;
+    curr->next = head;
+    head->prev = curr;
+    head = curr;
+    bottom = bottom->prev;
+    bottom->next = head;
+    head->prev = bottom;
+}
+
+void circleLL::update(int entry){
+    ListNode *curr = head;
+    while(curr->val != entry){
+        curr = curr->next;
+    }
+    curr->score += 1.00;
+    if(curr == head){
+        return;
+    }
+    if(curr == bottom){
+        head = bottom;
+        bottom = bottom->prev;
+        return;
+    }
+    curr->next->prev = curr->prev;
+    curr->prev->next = curr->next;
+    head->prev = curr;
+    bottom->next = curr;
+    curr->next = head;
+    curr->prev = bottom;
+    head = curr;
+}
+
+circleLL::~circleLL()
+{
+}
+
